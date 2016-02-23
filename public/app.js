c82fa68c67675cc47ae46cce6c81573556601dd4
Alex Wynter <awochna@email.arizona.edu>
2016-02-23 14:26:13 -0700

    Create an application shell.

--- public/app.js
+++ public/app.js
@@ -2,15 +2,83 @@
 
 var learnjs = {};
 
-learnjs.problemView = function (problemNumber) {
-  var view = $('.templates .problem-view').clone();
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem () { return ____; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem () { return 42 === 6 * ____; }"
+  }
+];
+
+learnjs.applyObject = function (obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
+
+learnjs.flashElement = function (elem, content) {
+  elem.fadeOut('fast', function () {
+    elem.html(content);
+    elem.fadeIn();
+  });
+};
+
+learnjs.template = function (name) {
+  return $('.templates .' + name).clone();
+};
+
+learnjs.landingView = function () {
+  return learnjs.template('landing-view');
+};
+
+learnjs.buildCorrectFlash = function (problemNum) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a');
+  if (problemNum < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNum + 1));
+  } else {
+    link.attr('href', '');
+    link.text("You're Finished!");
+  }
+  return correctFlash;
+}
+
+learnjs.problemView = function (data) {
+  var problemNumber = parseInt(data, 10);
+  var view = learnjs.template('problem-view');
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
+  
+  function checkAnswer() {
+    var answer = view.find('.answer').val();
+    var test = problemData.code.replace('____', answer) + '; problem();';
+    return eval(test);
+  }
+  
+  function checkAnswerClick() {
+    if (checkAnswer()) {
+      var correctFlash = learnjs.buildCorrectFlash(problemNumber);
+      learnjs.flashElement(resultFlash, correctFlash);
+    } else {
+      learnjs.flashElement(resultFlash, 'Incorrect!');
+    }
+    return false;
+  }
+  
+  view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject(problemData, view);
   return view;
 }
 
 learnjs.showView = function (hash) {
   var routes = {
     '#problem': learnjs.problemView,
+    '#': learnjs.landingView,
+    '': learnjs.landingView,
   };
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
